259adb6a1e541b861ff76dcd8ac65d6dfc109212
Joseph Skrzysowski <josephskrzysowski@gmail.com>
2018-06-23 17:26:56 -0400

    Setting up server, added handlebars engine, working on middle ware and partials next

--- public/github.js
+++ public/github.js
@@ -2,15 +2,15 @@
 class GitUser {
   constructor() {
 	this.hub_client_id='5af34cfbfc1ad6036cad';
-	this.hub_client_secret=
+	this.hub_client_secret='3f7e421db5bbd6207f41be0306318593d6ec06cf';
 	this.repos_count = 5;
 	this.repos_sort = 'created: asc';
   }
 
   async getUser(user) { //
 	let hubUrl = `https://api.github.com/users/${user}?client_id=${this.hub_client_id}&client_secret=${this.hub_client_secret}`;
-	// let hubRepoUrl = `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&$sort=${this.repos_sort}&client_id=${this.hub_client_id}&client_secret=${this.hub_client_secret}`;
-	let hubRepoUrl = `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&$sort=pinned&client_id=${this.hub_client_id}&client_secret=${this.hub_client_secret}`;
+	 let hubRepoUrl = `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&$sort=${this.repos_sort}&client_id=${this.hub_client_id}&client_secret=${this.hub_client_secret}`;
+	//let hubRepoUrl = `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&$sort=pinned&client_id=${this.hub_client_id}&client_secret=${this.hub_client_secret}`;
 
 	// USE URL TO FETCH DATA
 	const hubProfileResponse = await fetch(hubUrl);
@@ -26,4 +26,3 @@ class GitUser {
 	}
   }
 };
-///////////////////////////////////////////////////////////////////////////////////////////////
